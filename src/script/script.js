8623fdbf9491b124338b7f7d86f5fb609914d54d
João Victor <joaovictor160x@gmail.com>
2025-05-07 23:40:11 -0300

    algumas melhoras

--- src/script/script.js
+++ src/script/script.js
@@ -1,66 +1,68 @@
-// // const data = await getData();
-// let dataDay = [];
-// async function getData() {
-//   try {
-//     const response = await fetch("data.json");
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     dataDay = await response.json();
-
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-// const div_day = document.querySelectorAll(".div_day");
-// const spans = document.querySelectorAll(".span__value");
-// dataDay.forEach((day, index) => {
-
-//   div_day[index].style.Height = day.amount * 2
-// });
-
-// getData();
-
 let dataDay = [];
 
 async function getData() {
   try {
-    const response = await fetch("data.json");
+    const response = await fetch("../../data.json");
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     dataDay = await response.json();
 
+    renderWeekDays(dataDay);
     // Após carregar os dados, chamamos a função para atualizar a UI
-    updateUI();
   } catch (error) {
     console.log(error.message);
   }
 }
 
-const div_day = document.querySelectorAll(".div_day");
-const spans = document.querySelectorAll(".span__value");
-const week_day = document.querySelectorAll(".week-day");
+function updateUI(data) {
+  const div_day = document.querySelectorAll(".div_day");
+  const spans = document.querySelectorAll(".span__value");
+  const week_day = document.querySelectorAll(".week-day");
 
-function updateUI() {
-  dataDay.forEach((day, index) => {
-    if (div_day[index]) {
-      div_day[index].style.height = `${day.amount * 2}px`;
-      ("px");
-    }
-    if (spans[index]) {
-      spans[index].textContent = `$${day.amount.toFixed(2)}`; // Exibe o valor no span correspondente
-    }
+  data.forEach((day, index) => {
+    div_day[index].style.height = `${day.amount * 2}px`;
+    ("px");
+
+    spans[index].textContent = `$${day.amount.toFixed(2)}`; // Exibe o valor no span correspondente
   });
 
   week_day.forEach((li, index) => {
     li.addEventListener("mouseenter", () => {
-      spans[index].style.display = "block";
+    spans[index].classList.add("show");
     });
     li.addEventListener("mouseleave", () => {
-      spans[index].style.display = "none";
+    spans[index].classList.remove("show");
     });
   });
+}
+
+function renderWeekDays(data) {
+  const container = document.querySelector(".weekly-expenses");
+
+  data.forEach((item) => {
+    const li = document.createElement("li");
+    li.className = "week-day";
+
+    const span = document.createElement("span");
+    span.className = `span__value span__${item.day}`;
+    span.textContent = `$${item.day}`;
+
+    const div = document.createElement("div");
+    div.className = `div_${item.day} div_day`;
+
+    const p = document.createElement("p");
+    p.className = "day";
+    p.textContent = item.day;
+
+    li.appendChild(span);
+    li.appendChild(div);
+    li.appendChild(p);
+    container.appendChild(li);
+  });
+
+  updateUI(data);
 }
 
 // Chamamos a função para buscar os dados e atualizar a UI
